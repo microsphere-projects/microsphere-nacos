5d251cf55a7363fa802e6a0f97512ce0d08aac4f
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-22 11:06:14 +0800

    Migration microsphere-nacos-openapi

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    // Include 'plugins build' to define convention plugins.
+    includeBuild("build-logic")
+}
+
+plugins {
+    // Apply the foojay-resolver plugin to allow automatic download of JDKs
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+rootProject.name = "microsphere-nacos"
+
+include(
+    "microsphere-nacos-openapi"
+)
