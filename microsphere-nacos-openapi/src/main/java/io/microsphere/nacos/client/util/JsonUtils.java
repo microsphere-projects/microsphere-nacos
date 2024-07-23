98f50d096d66b3a2b46ee0413b766dac6fcc65eb
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-23 20:15:59 +0800

    Update JsonUtils.java

--- microsphere-nacos-openapi/src/main/java/io/microsphere/nacos/client/util/JsonUtils.java
+++ microsphere-nacos-openapi/src/main/java/io/microsphere/nacos/client/util/JsonUtils.java
@@ -16,6 +16,7 @@
  */
 package io.microsphere.nacos.client.util;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
@@ -27,6 +28,43 @@ import java.util.StringJoiner;
  * @since 1.0.0
  */
 public abstract class JsonUtils {
+
+    private static final String SEPARATOR = ",";
+
+    private static final String JSON_START = "{";
+
+    private static final String JSON_END = "}";
+
+    private static final String ARRAY_JSON_START = "[";
+
+    private static final String ARRAY_JSON_END = "]";
+
+    private static final String EMPTY_JSON = JSON_START + JSON_END;
+
+    private static final String EMPTY_ARRAY_JSON = ARRAY_JSON_START + ARRAY_JSON_END;
+
+
+    public static String toJSON(Collection<?> elements) {
+        int size = elements == null ? 0 : elements.size();
+        if (size < 1) {
+            return EMPTY_ARRAY_JSON;
+        }
+        StringJoiner jsonBuilder = new StringJoiner(SEPARATOR, ARRAY_JSON_START, ARRAY_JSON_END);
+
+        for (Object element : elements) {
+            jsonBuilder.add(toJSON(element));
+        }
+
+        return jsonBuilder.toString();
+    }
+
+    public static String toJSON(Object object) {
+        if (object instanceof Collection) {
+            return toJSON((Collection<?>) object);
+        } else if (object instanceof Map) {
+            return toJSON((Map<String, String>) object);
+        }
+    }
 
     public static String toJSON(List<Map<String, String>> maps) {
         int size = maps == null ? 0 : maps.size();
@@ -49,26 +87,21 @@ public abstract class JsonUtils {
      * @param map {@link Map}
      * @return non-null
      */
-    public static String toJSON(Map<String, String> map) {
+    public static String toJSON(Map<?, ?> map) {
         int size = map == null ? 0 : map.size();
         if (size < 1) {
-            return "{}";
+            return EMPTY_JSON;
         }
 
         StringBuilder jsonBuilder = new StringBuilder(32 * size);
 
         int cursor = 0;
         jsonBuilder.append("{");
-        for (Map.Entry<String, String> entry : map.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
+        for (Map.Entry<?, ?> entry : map.entrySet()) {
+            Object key = entry.getKey();
+            Object value = entry.getValue();
             jsonBuilder.append('"')
-                    .append(key)
-                    .append('"')
-                    .append(':')
-                    .append('"')
-                    .append(value)
-                    .append('"');
+                    .append(key).append('"').append(':').append('"').append(value).append('"');
             if (++cursor < size) {
                 jsonBuilder.append(",");
             }
