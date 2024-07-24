4755d2bf23c68fdd096cbee1c62bda2175c9776d
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-24 21:59:54 +0800

    Create ConfigClientV2Test.java

---
+++ microsphere-nacos-openapi/src/test/java/io/microsphere/nacos/client/v2/config/ConfigClientV2Test.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.nacos.client.v2.config;
+
+import io.microsphere.nacos.client.OpenApiTest;
+import org.junit.jupiter.api.Test;
+
+import static io.microsphere.nacos.client.v1.config.ConfigClientTest.TEST_CONFIG_CONTENT;
+import static io.microsphere.nacos.client.v1.config.ConfigClientTest.TEST_DATA_ID;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * The {@link ConfigClientV2} Test
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see ConfigClientV2
+ * @since 1.0.0
+ */
+public class ConfigClientV2Test extends OpenApiTest {
+
+    @Test
+    public void test() {
+        ConfigClientV2 client = new OpenApiConfigClientV2(this.openApiClient, this.nacosClientConfig);
+
+        assertTrue(client.publishConfigContent(TEST_NAMESPACE_ID, TEST_GROUP_NAME, TEST_DATA_ID, TEST_CONFIG_CONTENT));
+
+        String newContent = "New Content for testing...";
+        assertTrue(client.publishConfigContent(TEST_NAMESPACE_ID, TEST_GROUP_NAME, TEST_DATA_ID, newContent));
+    }
+}
