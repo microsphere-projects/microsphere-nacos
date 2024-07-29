67c5bd256497590571fa57cf39fa8538deb28990
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-29 20:26:51 +0800

    Update OpenApiNamespaceClientV2Test.java

--- microsphere-nacos-openapi/src/test/java/io/microsphere/nacos/client/v2/namespace/OpenApiNamespaceClientV2Test.java
+++ microsphere-nacos-openapi/src/test/java/io/microsphere/nacos/client/v2/namespace/OpenApiNamespaceClientV2Test.java
@@ -16,24 +16,9 @@
  */
 package io.microsphere.nacos.client.v2.namespace;
 
-import io.microsphere.nacos.client.OpenApiTest;
 import io.microsphere.nacos.client.common.namespace.NamespaceClient;
-import io.microsphere.nacos.client.common.namespace.model.Namespace;
 import io.microsphere.nacos.client.v1.namespace.OpenApiNamespaceClient;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
-import static io.microsphere.nacos.client.v1.namespace.OpenApiNamespaceClientTest.MODIFIED_NAMESPACE_DESC;
-import static io.microsphere.nacos.client.v1.namespace.OpenApiNamespaceClientTest.MODIFIED_NAMESPACE_NAME;
-import static io.microsphere.nacos.client.v1.namespace.OpenApiNamespaceClientTest.NAMESPACE_DESC;
-import static io.microsphere.nacos.client.v1.namespace.OpenApiNamespaceClientTest.NAMESPACE_ID;
-import static io.microsphere.nacos.client.v1.namespace.OpenApiNamespaceClientTest.NAMESPACE_NAME;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import io.microsphere.nacos.client.v1.namespace.OpenApiNamespaceClientTest;
 
 /**
  * {@link OpenApiNamespaceClientV2} Test
@@ -43,47 +28,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * @see OpenApiNamespaceClient
  * @since 1.0.0
  */
-public class OpenApiNamespaceClientV2Test extends OpenApiTest {
+public class OpenApiNamespaceClientV2Test extends OpenApiNamespaceClientTest {
 
-    private NamespaceClient client;
-
-    @BeforeEach
-    public void setup() {
-        this.client = new OpenApiNamespaceClientV2(this.openApiClient);
-        client.deleteNamespace(NAMESPACE_ID);
-    }
-
-    @Test
-    public void test() {
-        NamespaceClient client = this.client;
-
-        // Test getAllNamespace()
-        List<Namespace> namespaces = client.getAllNamespaces();
-        int namespacesSize = namespaces.size();
-        assertNotNull(namespaces);
-        assertTrue(namespacesSize > 0);
-        Namespace namespace = namespaces.get(0);
-        assertEquals("public", namespace.getNamespaceName());
-
-        // Test getNamespace()
-        namespace = client.getNamespace(NAMESPACE_ID);
-
-        // Not found
-        assertNull(namespace);
-
-        // Test createNamespace()
-        assertTrue(client.createNamespace(NAMESPACE_ID, NAMESPACE_NAME, NAMESPACE_DESC));
-        namespaces = client.getAllNamespaces();
-        namespace = namespaces.get(namespaces.size() - 1);
-        assertEquals(NAMESPACE_ID, namespace.getNamespaceId());
-        assertEquals(NAMESPACE_NAME, namespace.getNamespaceName());
-
-        // Test updateNamespace()
-        for (int i = 0; i < 5; i++) {
-            assertTrue(client.updateNamespace(NAMESPACE_ID, MODIFIED_NAMESPACE_NAME, MODIFIED_NAMESPACE_DESC));
-        }
-
-        // Test deleteNamespace()
-        assertTrue(client.deleteNamespace(NAMESPACE_ID));
+    @Override
+    protected NamespaceClient createNamespaceClient() {
+        return new OpenApiNamespaceClientV2(this.openApiClient);
     }
 }
