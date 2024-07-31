55ef9cd3ea0cd5874038668b6d1129988851ca7d
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-31 12:37:56 +0800

    Update OpenApiNamespaceClientV2Test.java

--- microsphere-nacos-openapi/src/test/java/io/microsphere/nacos/client/v2/namespace/OpenApiNamespaceClientV2Test.java
+++ microsphere-nacos-openapi/src/test/java/io/microsphere/nacos/client/v2/namespace/OpenApiNamespaceClientV2Test.java
@@ -32,6 +32,6 @@ public class OpenApiNamespaceClientV2Test extends OpenApiNamespaceClientTest {
 
     @Override
     protected NamespaceClient createNamespaceClient() {
-        return new OpenApiNamespaceClientV2(this.openApiClient);
+        return new OpenApiNamespaceClientV2(this.openApiClient, this.nacosClientConfig);
     }
 }
