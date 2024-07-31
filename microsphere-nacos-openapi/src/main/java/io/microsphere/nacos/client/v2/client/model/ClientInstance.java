5ef43ab0baeabe7231edd667cd37d5bb548a9da2
小马哥（mercyblitz） <mercyblitz@gmail.com>
2024-07-31 15:54:45 +0800

    Create ClientInstance.java

---
+++ microsphere-nacos-openapi/src/main/java/io/microsphere/nacos/client/v2/client/model/ClientInstance.java
@@ -0,0 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.microsphere.nacos.client.v2.client.model;
+
+import io.microsphere.nacos.client.common.discovery.model.BaseInstance;
+import io.microsphere.nacos.client.common.model.Model;
+
+/**
+ * The {@link BaseInstance} {@link Model} {@link Class} for client
+ *
+ * @author <a href="mailto:mercyblitz@gmail.com">Mercy<a/>
+ * @see BaseInstance
+ * @since 1.0.0
+ */
+public class ClientInstance extends BaseInstance {
+
+    private static final long serialVersionUID = -3106293456068549205L;
+}
